6626615db6a80cef34eece5e06bb3869b959c82b
FroggyKwa <ilya.laguxa@gmail.com>
2026-01-21 16:20:13 +1000

    connect to db

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,22 +1,26 @@
+from typing import Optional
+
 from pydantic_settings import SettingsConfigDict, BaseSettings
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".docker/.env")
 
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_DB: str
-    POSTGRES_HOST: str
-    DATABASE_URL: str | None = None
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+    DATABASE_NAME: str
+    DATABASE_HOST: str
+    DATABASE_PORT: Optional[str] = "5432"
+    DATABASE_URL: Optional[str] = None
     DEBUG: bool = False
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
         if not self.DATABASE_URL:
             self.DATABASE_URL = (
-                f"postgresql+psycopg2://{self.POSTGRES_USER}:"
-                f"{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}/{self.POSTGRES_DB}"
+                f"postgresql+psycopg2://{self.DATABASE_USER}:{self.DATABASE_PASSWORD}"
+                f"@{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_NAME}"
             )
 
 
