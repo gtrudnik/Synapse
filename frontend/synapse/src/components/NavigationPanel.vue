ed9f323b0228f0c36bd0ccae411c19a74c2d5591
Pavel <rudnikcode@gmail.com>
2026-01-23 00:09:10 +1000

    feat: add pages

--- frontend/synapse/src/components/NavigationPanel.vue
+++ frontend/synapse/src/components/NavigationPanel.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import logo from '../assets/synapse.png';
 
 const isAuthenticated = ref(true)
 
@@ -13,19 +14,23 @@ function logout() {
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light px-3 py-2 shadow-sm">
     <div class="container-fluid">
+      <img :src="logo" alt="Logo" class="navbar-nav me-auto mb-2 mb-lg-0" style="width: 24px; height: auto;" />
       <!-- Навигационные ссылки -->
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <router-link class="nav-link" to="/">Главная</router-link>
+          <router-link class="nav-link" to="/search">Поиск друга</router-link>
         </li>
-        <li class="nav-item" v-if="isAuthenticated">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/form">Анкеты</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/match">Мэтчи</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/dialog">Диалоги</router-link>
+        </li>
+        <li class="nav-item">
           <router-link class="nav-link" to="/profile">Профиль</router-link>
-        </li>
-        <li class="nav-item" v-if="!isAuthenticated">
-          <router-link class="nav-link" to="/login">Войти</router-link>
-        </li>
-        <li class="nav-item" v-if="!isAuthenticated">
-          <router-link class="nav-link" to="/register">Регистрация</router-link>
         </li>
       </ul>
 
--- frontend/synapse/src/router/index.ts
+++ frontend/synapse/src/router/index.ts
@@ -4,10 +4,20 @@ import { createRouter, createWebHistory } from 'vue-router'
 // Импортируйте компоненты страниц
 import AuthPage from '../pages/AuthPage.vue'
 import RegisterPage from '../pages/RegisterPage.vue'
+import SearchPage from '../pages/SearchPage.vue'
+import FormPage from '../pages/FormPage.vue'
+import MatchPage from '../pages/MatchPage.vue'
+import DialogPage from '../pages/DialogPage.vue'
+import ProfilePage from '../pages/ProfilePage.vue'
 
 const routes = [
   { path: '/', component: AuthPage },
   { path: '/register', component: RegisterPage },
+  { path: '/search', component: SearchPage },
+  { path: '/form', component: FormPage },
+  { path: '/match', component: MatchPage },
+  { path: '/dialog', component: DialogPage },
+  { path: '/profile', component: DialogPage },
 ]
 
 const router = createRouter({
