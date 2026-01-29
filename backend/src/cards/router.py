7f3b060c2ad57a3e60efa868ef50a24c2d4de36b
FroggyKwa <ilya.laguxa@gmail.com>
2026-01-29 17:06:15 +1000

    fix card method permissions

--- backend/src/cards/router.py
+++ backend/src/cards/router.py
@@ -1,9 +1,13 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
+from src.auth.dependencies import get_current_user
 from src.database.core import get_db
 from src.cards.service import CardService
 from src.cards.schemas import CardCreate, CardRead
+from src.users.schemas import UserRead
 
 router = APIRouter()
 
@@ -11,14 +15,14 @@ router = APIRouter()
 @router.post("/", response_model=CardRead)
 def create_card(
     card_data: CardCreate,
-    user_id: int,
+    current_user: Annotated[UserRead, Depends(get_current_user)],
     db: Session = Depends(get_db),
 ):
     service = CardService(db)
-    return service.create_card(user_id=user_id, card_data=card_data)
+    return service.create_card(user_id=current_user.id, card_data=card_data)
 
 
-@router.get("/", response_model=list[CardRead])
+@router.get("/all", response_model=list[CardRead])
 def get_all_cards(
     db: Session = Depends(get_db),
 ):
@@ -26,10 +30,10 @@ def get_all_cards(
     return service.get_all_cards()
 
 
-@router.get("/{user_id}", response_model=list[CardRead])
+@router.get("/", response_model=list[CardRead])
 def get_user_cards(
-    user_id: int,
+    current_user: Annotated[UserRead, Depends(get_current_user)],
     db: Session = Depends(get_db),
 ):
     service = CardService(db)
-    return service.get_user_cards(user_id)
+    return service.get_user_cards(current_user.id)
